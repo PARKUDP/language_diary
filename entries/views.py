7ef39cefaf1d7598bc4c96dab037e742b0406dc0
e225752 <e225752@ie.u-ryukyu.ac.jp>
2024-08-03 11:00:54 +0900

    ある程度完成

--- entries/views.py
+++ entries/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+# entries/views.py
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Entry
+from .forms import EntryForm
+from django.contrib.auth.decorators import login_required
 
 def entry_list(request):
     entries = Entry.objects.all().order_by('-date_posted')
-    return render(request, 'entries/entry_list.html', {'entires': entries})
+    return render(request, 'entries/entry_list.html', {'entries': entries})
 
 def entry_detail(request, pk):
-    entry = Entry.objects.get(pk=pk)
+    entry = get_object_or_404(Entry, pk=pk)
     return render(request, 'entries/entry_detail.html', {'entry': entry})
+
+@login_required
+def entry_create(request):
+    if request.method == "POST":
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('entry_list')
+    else:
+        form = EntryForm()
+    return render(request, 'entries/entry_form.html', {'form': form})
+
+@login_required
+def entry_update(request, pk):
+    entry = get_object_or_404(Entry, pk=pk)
+    if request.method == "POST":
+        form = EntryForm(request.POST, instance=entry)
+        if form.is_valid():
+            form.save()
+            return redirect('entry_detail', pk=pk)
+    else:
+        form = EntryForm(instance=entry)
+    return render(request, 'entries/entry_form.html', {'form': form})
+
+@login_required
+def entry_delete(request, pk):
+    entry = get_object_or_404(Entry, pk=pk)
+    if request.method == "POST":
+        entry.delete()
+        return redirect('entry_list')
+    return render(request, 'entries/entry_confirm_delete.html', {'entry': entry})
