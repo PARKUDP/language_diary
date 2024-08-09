bb1d52768bd885399ae4ac995e762ab33bc2eaa2
e225752 <e225752@ie.u-ryukyu.ac.jp>
2024-08-10 02:23:41 +0900

    entry_form.css added

--- entries/forms.py
+++ entries/forms.py
@@ -5,3 +5,7 @@ class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['title', 'content']
+        labels = {
+            'title': 'タイトル:',
+            'content': '本文:'
+        }
